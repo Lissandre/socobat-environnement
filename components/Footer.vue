83681069b7e5c851ddd76f17de0495bf32284f76
Lissandre <lissandre.pasdeloup@hetic.net>
2020-02-26 12:26:20 +0100

    :clown_face: Nav & footer

---
+++ components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="footer">
+    <span>
+      © Copyright 2020 Shaftee | Tous droits réservés
+    </span>
+    <nuxt-link to="/mentions-legales">
+      Mentions Légales
+    </nuxt-link>
+    <nuxt-link to="/confidentialite">
+      Politique de Confidentialité
+    </nuxt-link>
+    <nuxt-link to="/contact">
+      Contact
+    </nuxt-link>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus">
+.footer
+  width 100%
+  position absolute
+  bottom 0
+  display flex
+  justify-content center
+  padding 10px 0
+
+.footer a
+  color $green
+  &:before
+    content '|'
+    margin-left 4px
+    color $black
+    cursor text
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <nuxt-link to="/">
+        SOCOBAT
+      </nuxt-link>
+    </div>
+    <div class="linksList">
+      <nuxt-link to="/">
+        Accueil
+      </nuxt-link>
+      <nuxt-link to="/location-bennes">
+        Location de bennes
+      </nuxt-link>
+      <nuxt-link to="/centre-de-tri">
+        Centre de tri
+      </nuxt-link>
+      <nuxt-link to="/services">
+        Services
+      </nuxt-link>
+      <nuxt-link to="/devis">
+        Votre devis
+      </nuxt-link>
+      <nuxt-link to="/a-propos">
+        À propos
+      </nuxt-link>
+      <nuxt-link to="/contact">
+        Contact
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus">
+.nav
+  position fixed
+  width 100%
+  padding 20px 5%
+  display flex
+  justify-content space-between
+
+.linksList a
+  color $green
+  &:not(:first-child)
+    margin-left 15px
+</style>
