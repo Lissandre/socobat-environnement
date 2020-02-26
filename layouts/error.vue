e4545dc02871061d08e30197f5650a616aeb8a5d
Lissandre <lissandre.pasdeloup@hetic.net>
2020-02-26 15:26:50 +0100

    :goal_net: Catching errors

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">
+      404 - Page non trouvée
+    </h1>
+    <h1 v-else>
+      Une erreur s'est produite
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
