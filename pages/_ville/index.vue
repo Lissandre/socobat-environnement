60a3060bce2aa30da5d3e7ce91323bc838f77925
Lissandre <lissandre.pasdeloup@hetic.net>
2020-02-28 14:45:50 +0100

    test duplicate content

--- pages/_ville/index.vue
+++ pages/_ville/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="city">
-    {{ ville }} est une belle ville.
+    Dpann PC {{ ville }} est une société de réparation d'ordinateur à {{ ville }} qui est spécialisé dans l'assistance, la formation et le dépannage informatique sur {{ ville }}. Dépannage ordinateur, installation, réparation, configuration, maintenance, conseil ...
+    <br>
+    Vous rencontrez des soucis informatiques à {{ ville }} ? Votre ordinateur est devenu lent et vous êtes assailli de publicités douteuses ? Une page bloque l'accès à votre PC vous demandant de payer pour pouvoir le débloquer ? Vous pensez avoir un virus ou des logiciels malveillants sur votre PC ? Votre ordinateur ne démarre plus ou il s’essouffle ? Votre réseau ne répond plus ? Vous avez un problème d'installation ou de connexion sur internet ? Vous manquez d'expérience et avez besoin de formation ?
+    <br>
+    Dpann PC {{ ville }} vous apporte une solution à tous vos problèmes informatiques.
   </div>
 </template>
 
